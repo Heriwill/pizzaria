fbe0094e2279d3ac69a9f7a99732338c94f211d7
Heriwill <willian.herisson@gamil.com>
2024-07-29 20:31:45 -0300

    Criação da primeira

---
+++ src/main/java/com/itb/mif3an/pizzaria/model/Produto.java
@@ -0,0 +1,22 @@
+package com.itb.mif3an.pizzaria.model;
+
+import javax.swing.*;
+public class Produto {
+
+        public Long id;
+
+        public String nome;
+
+        public String tipo;
+
+        public String descricao;
+
+        public double precoVenda;
+
+        public int quantidadeEstoque;
+
+        public boolean codStatus;
+
+
+
+}
