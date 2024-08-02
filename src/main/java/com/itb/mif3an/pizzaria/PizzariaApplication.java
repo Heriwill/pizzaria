8da55235c33683ae83af1a2aa141cd08f6d70253
Heriwill <willian.herisson@gamil.com>
2024-08-01 21:08:23 -0300

    Lógica de validação dos modelos concluida 'Encapsulamento'

--- src/main/java/com/itb/mif3an/pizzaria/PizzariaApplication.java
+++ src/main/java/com/itb/mif3an/pizzaria/PizzariaApplication.java
@@ -1,5 +1,7 @@
 package com.itb.mif3an.pizzaria;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.itb.mif3an.pizzaria.model.Produto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,21 @@ public class PizzariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzariaApplication.class, args);
-	}
+
+		Produto p = new Produto();
+		//p.precoCompra = -45.00;
+
+		//System.out.println("Preço de compra " + p.precoCompra);
+
+//p.setPrecoCompra(-56.0);
+//System.out.println("preço de compra " + p.getPrecoCompra());
+
+ 		p.setNome ("Pizza de calabresa tamanho 6");
+		p.setPrecoCompra((-56.0));
+
+		if(!p.validarProduto()){
+	System.out.println(p.getMensagemErro());
+}
+      }
 
 }
--- src/main/java/com/itb/mif3an/pizzaria/model/Produto.java
+++ src/main/java/com/itb/mif3an/pizzaria/model/Produto.java
@@ -3,20 +3,125 @@ package com.itb.mif3an.pizzaria.model;
 import javax.swing.*;
 public class Produto {
 
-        public Long id;
 
-        public String nome;
+        //Encapsulamento: proteger meus atributos do acesso desordenado, pode estar ligado
+        //ás questoes de validação
 
-        public String tipo;
+        //Para obter o encapsulamento temos que seguir alguns passos:
+        //1° Passo: Trabalhar os modificadores de acesso
+        //Temos três:
+        //public -> Acesso livre para todas as classes
+        //private -> Acesso permitido apenas dentro da classes
+        // protect -> Acesso permitido apenas para as classes filhas (Herança)
 
-        public String descricao;
+        //Enfim, deixar os atributos private ou protected
 
-        public double precoVenda;
+        //2° Passo: criar métodos de acesso aos atributos, uma prática muito utilizada
+        // é criar os métodos SETTER'S E GETTER'S
+        //SET -> atribuir a informação
+        //GET -> recuperar a informação
+        private Long id;
 
-        public int quantidadeEstoque;
+        private String nome;
 
-        public boolean codStatus;
+        private String tipo;
 
+        private String descricao;
 
+        private double precoVenda;
 
+        private double precoCompra;
+
+        private int quantidadeEstoque;
+
+        private boolean codStatus;
+
+        private String mensagemErro = "";
+        private boolean isValid  = true;
+
+        public Produto() {
+        }
+
+        public void setId(Long id){ //void = "mudo", o método não tem retorno
+                this.id = id; //this é referencia do atributo da classe id; separa o atributo da variavel q tem o mesmo nome
+        }
+
+        //comando ALT + INSERT para gerar todos os getters e setters
+        public Long getId(){
+                return id;
+        }
+
+        public String getNome() {
+                return nome;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+        public String getTipo() {
+                return tipo;
+        }
+
+        public void setTipo(String tipo) {
+                this.tipo = tipo;
+        }
+
+        public String getDescricao() {
+                return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+                this.descricao = descricao;
+        }
+
+        public double getPrecoVenda() {
+                return precoVenda;
+        }
+
+        public void setPrecoVenda(double precoVenda) {
+                this.precoVenda = precoVenda;
+        }
+
+        public double getPrecoCompra() {
+                return precoCompra;
+        }
+
+        public void setPrecoCompra(double precoCompra) {
+                this.precoCompra = precoCompra;
+        }
+
+        public int getQuantidadeEstoque() {
+                return quantidadeEstoque;
+        }
+
+        public void setQuantidadeEstoque(int quantidadeEstoque) {
+                this.quantidadeEstoque = quantidadeEstoque;
+        }
+
+        public boolean isCodStatus() {
+                return codStatus;
+        }
+
+        public void setCodStatus(boolean codStatus) {
+                this.codStatus = codStatus;
+        }
+
+        public String getMensagemErro() {
+                return mensagemErro;
+        }
+
+        public boolean validarProduto() {
+
+            if (nome == null || nome.isEmpty()){
+                    mensagemErro += "O nome do produto é obrigatório:";
+                    isValid = false;
+            }
+                if (precoCompra < 0){
+                        precoCompra = 0;
+                       mensagemErro += "O preço do produto deve ser maior que zero:";
+                       isValid = false;
+                }
+                return isValid;
+        }
 }
